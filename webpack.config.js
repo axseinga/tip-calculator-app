63e0e44cbc3a8aa2bfb48c3a831eca8f9d163a0d
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2021-07-28 13:26:33 +0100

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/js/index.js",
+
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        publicPath: "",
+        filename: "bundle.js",
+    },
+
+    mode: "development",
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: `./index.html`,
+            filename: "index.html",
+        }),
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: "babel-loader",
+            },
+
+            {
+                test: /\.s[ac]ss$/i,
+                use: ["style-loader", "css-loader", "sass-loader"],
+            },
+        ],
+    },
+};
