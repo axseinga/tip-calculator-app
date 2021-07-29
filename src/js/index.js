a369d947603b5fa7c3b47f978558122274135a7e
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2021-07-29 09:51:36 +0100

    webpack config working

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,3 @@
+import("./../sass/main.scss");
+
 console.log("is working");
--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,16 @@ module.exports = {
 
             {
                 test: /\.s[ac]ss$/i,
-                use: ["style-loader", "css-loader", "sass-loader"],
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            implementation: require.resolve("dart-sass"),
+                        },
+                    },
+                ],
             },
         ],
     },
